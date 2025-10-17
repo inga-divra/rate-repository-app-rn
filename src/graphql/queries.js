08affe92ebfc232030eac51a06024c1d3afd53ca
inga-divra <106383378+inga-divra@users.noreply.github.com>
2025-10-17 19:23:44 +0300

    feat(graphql): add GET_SINGLE_REPO query for fetching single repo.

--- src/graphql/queries.js
+++ src/graphql/queries.js
@@ -28,3 +28,20 @@ export const ME = gql`
     }
   }
 `;
+
+export const GET_SINGLE_REPO = gql`
+  query GetSingleRepo($id: ID!) {
+    repository(id: $id) {
+      id
+      fullName
+      description
+      language
+      forksCount
+      stargazersCount
+      ratingAverage
+      reviewCount
+      ownerAvatarUrl
+      url
+    }
+  }
+`;
