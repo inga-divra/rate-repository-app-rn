0d6bee6695b5fcabefbfeb90db4fb60e77fcefec
inga-divra <106383378+inga-divra@users.noreply.github.com>
2025-10-23 14:59:07 +0300

    feat(graphql): add createUser mutation for user registration.

--- src/graphql/queries.js
+++ src/graphql/queries.js
@@ -59,3 +59,12 @@ export const GET_SINGLE_REPO = gql`
     }
   }
 `;
+
+export const CREATE_USER = gql`
+  mutation CreateUser($username: String!, $password: String!) {
+    createUser(user: { username: $username, password: $password }) {
+      id
+      username
+    }
+  }
+`;
