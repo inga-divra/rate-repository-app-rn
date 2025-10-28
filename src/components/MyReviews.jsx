938fdaf876130e892471e6f928a9d7832d8c2a21
inga-divra <106383378+inga-divra@users.noreply.github.com>
2025-10-28 11:41:18 +0200

    feat(my-reviews): finish MyReviews component for Exercise 10.26: review actions.

--- src/components/MyReviews.jsx
+++ src/components/MyReviews.jsx
@@ -1,8 +1,11 @@
-import { FlatList, View, StyleSheet } from 'react-native';
+import { FlatList, View, StyleSheet, Alert, Platform } from 'react-native';
 import Text from './Text'
 //import theme from '../../assets/styles/theme';
 import useCurrentUserReviews from '../hooks/useCurrentUserReviews';
 import SingleReview from './SingleReview';
+import { useNavigate } from 'react-router-native';
+import { useMutation } from '@apollo/client';
+import { DELETE_REVIEW } from '../graphql/mutations'
 
 
 const styles = StyleSheet.create({
@@ -16,7 +19,9 @@ const ItemSeparator = () => <View style={styles.separator} />;
 
 const MyReviews = () => {
 
-    const { reviews, loading, error } = useCurrentUserReviews()
+    const { reviews, loading, error, refetch } = useCurrentUserReviews()
+    const [deleteReview] = useMutation(DELETE_REVIEW)
+    const nav = useNavigate()
 
     if (loading) {
         return <Text>Loading...</Text>
@@ -25,6 +30,51 @@ const MyReviews = () => {
         return <Text>{String(error.message)}</Text>
     }
 
+    /* HANDLE VIEW REPO */
+    const handleViewRepo = (repoId) => {
+        if (repoId) {
+            nav(`/repository/${repoId}`)
+        }
+    }
+
+    /* HANDLE DELETE REVIEW */
+    const handleDeleteReview = async (reviewId) => {
+
+
+        /* WEB */
+        if (Platform.OS === 'web') {
+            const ok = window.confirm('Delete review\n\nAre you sure you want to delete this review?')
+            if (!ok) {
+                return
+            }
+            try {
+                await deleteReview({ variables: { id: reviewId } })
+                await refetch()
+            } catch (error) {
+                console.log(error?.message);
+            }
+            return
+        }
+        /* MOB */
+        Alert.alert('Delete review', 'Are you sure you want to delete this review?', [
+            {
+                text: 'Cancel',
+                style: 'cancel',
+            },
+            {
+                text: 'Delete',
+                onPress: async () => {
+                    try {
+                        await deleteReview({ variables: { id: reviewId } })
+                        await refetch()
+                    } catch (error) {
+                        console.log(error?.message);
+                    }
+                }
+            },
+        ]);
+
+    }
 
     // Get the nodes from the edges array
     const reviewNodes = reviews ? reviews.edges.map(edge => edge.node) : [];
@@ -32,7 +82,9 @@ const MyReviews = () => {
         <View >
             <FlatList
                 data={reviewNodes}
-                renderItem={({ item }) => <SingleReview item={item} showRepoName={true} />}
+                renderItem={({ item }) => {
+                    return <SingleReview item={item} showRepoName={true} onViewRepo={handleViewRepo} onDeleteReview={handleDeleteReview} />
+                }}
                 keyExtractor={item => item.id}
                 ItemSeparatorComponent={ItemSeparator}
             />
--- src/hooks/useCurrentUserReviews.js
+++ src/hooks/useCurrentUserReviews.js
@@ -2,11 +2,11 @@ import { useQuery } from '@apollo/client';
 import { GET_CURRENT_USER } from '../graphql/queries';
 
 const useCurrentUserReviews = () => {
-  const { data, loading, error } = useQuery(GET_CURRENT_USER, {
+  const { data, loading, error, refetch } = useQuery(GET_CURRENT_USER, {
     variables: { includeReviews: true },
     fetchPolicy: 'cache-and-network',
   });
   const reviews = data?.me?.reviews;
-  return { reviews, loading, error };
+  return { reviews, loading, error, refetch };
 };
 export default useCurrentUserReviews;
